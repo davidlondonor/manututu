2ff4b2534f21b5d6f5c7896ef06db1accfd2797e
david <davidlucky@gmail.com>
2022-06-26 13:19:50 -0500

    add slider

--- components/Slider.js
+++ components/Slider.js
@@ -2,7 +2,25 @@ import { motion } from "framer-motion";
 import { useState, useRef, UseEffect } from "react";
 
 const Slider = () => {
-	return <div>Hola</div>;
+	return (
+		<>
+			<motion.div className="carousel" animate={{ x: 50 }}>
+				<motion.div className="inner-carousel">
+					{/* {images.map((image, index) => {
+						return (
+							<motion.div className="item">
+								<img
+									src={"/denim/connection-denim-01.jpeg"}
+									alt="Description"
+								/>
+							</motion.div>
+						);
+					})} */}
+				</motion.div>
+			</motion.div>
+			<h1>Hola</h1>
+		</>
+	);
 };
 
 export default Slider;
