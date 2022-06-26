e5a518964da877ecf1fa1300331b7def31a74553
david <davidlucky@gmail.com>
2022-06-26 13:54:23 -0500

    add rules in to eslint

---
+++ components/Gallery/ImagesGallery.js
@@ -0,0 +1,9 @@
+import image1 from "../../public/denim/connection-denim.jpeg";
+import image2 from "../../public/denim/connection-denim-01.jpeg";
+import image3 from "../../public/denim/connection-denim-02.jpeg";
+import image4 from "../../public/denim/connection-denim-03.jpeg";
+import image5 from "../../public/denim/connection-denim-04.jpeg";
+import image6 from "../../public/denim/connection-denim-05.jpeg";
+import image7 from "../../public/denim/connection-denim-06.jpeg";
+
+export default [image1, image2, image3, image4, image5, image6, image7];
--- components/Slider.js
+++ components/Slider.js
@@ -1,21 +1,26 @@
 import { motion } from "framer-motion";
 import { useState, useRef, UseEffect } from "react";
+import Image from "next/image";
+import images from "../components/Gallery/ImagesGallery";
 
 const Slider = () => {
+	console.log(images);
 	return (
 		<>
 			<motion.div className="carousel" animate={{ x: 50 }}>
 				<motion.div className="inner-carousel">
-					{/* {images.map((image, index) => {
+					{images.map((image, index) => {
 						return (
-							<motion.div className="item">
-								<img
-									src={"/denim/connection-denim-01.jpeg"}
+							<motion.div className="item" key={2}>
+								<Image
+									src={image}
 									alt="Description"
+									width={100}
+									height={100}
 								/>
 							</motion.div>
 						);
-					})} */}
+					})}
 				</motion.div>
 			</motion.div>
 			<h1>Hola</h1>
