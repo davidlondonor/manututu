05ef07aa18134bbac018706ddd50a496c9eeeb37
david <davidlucky@gmail.com>
2022-06-29 19:38:14 -0500

    add carrusel

--- components/Slider.js
+++ components/Slider.js
@@ -6,12 +6,12 @@ import images from "../components/Gallery/ImagesGallery";
 const Slider = () => {
 	console.log(images);
 	return (
-		<>
+		<main className="ml-20 mr-20">
 			<motion.div
 				className="carousel cursor-grab overflow-hidden"
 				animate={{ x: 50 }}
 			>
-				<motion.div className="inner-carousel flex">
+				<motion.div drag="x" className="inner-carousel flex">
 					{images.map((image, index) => {
 						return (
 							<motion.div
@@ -19,11 +19,11 @@ const Slider = () => {
 								key={2}
 							>
 								<Image
-									className="rounded-3xl"
+									className="rounded-3xl pointer-events-none"
 									src={image}
 									alt="Description"
-									width={1000}
-									height={1400}
+									width={300}
+									height={440}
 								/>
 							</motion.div>
 						);
@@ -31,7 +31,7 @@ const Slider = () => {
 				</motion.div>
 			</motion.div>
 			<h1>Hola</h1>
-		</>
+		</main>
 	);
 };
 
